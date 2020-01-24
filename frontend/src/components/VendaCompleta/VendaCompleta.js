c1c0091b318f31db5f50d7715c09855840ed95fb
marcosrodriigues <marcos.rodriiigues@gmail.com>
2020-01-23 21:08:05 -0300

    Criaçao dos arquivos de venda completa

---
+++ frontend/src/components/VendaCompleta/VendaCompleta.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function VendaCompleta({ }) {
+    return (
+        <div className="col-sm-12 mt-25">
+            <p className="description strong mb-25">{analises.title}</p>
+            <table className="table">
+
+            </table>
+        </div>
+    )
+}
+
+export default VendaCompleta

32fc13035d53f0c98afdec6e3c6d5c46054f4f6f
marcosrodriigues <marcos.rodriiigues@gmail.com>
2020-01-25 20:44:52 -0300

    Updates

--- frontend/src/pages/dashboard/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Dashboard(props) {
-    return (
-        <>Dashboard works!</>
-    )
-}
-
-export default Dashboard;
--- frontend/src/util/Util.js
+++
@@ -1,5 +0,0 @@
-function formatFloat(value, fixed)  {
-    return value.toFixed(fixed).replace('.', ',')
-}
-
-export default formatFloat;
